ad00d6f269cd0908b1abc2cd771febaa0875ff99
Jakob Kogler <jakob.kogler@gmail.com>
2020-03-06 11:41:01 +0100

    Use indices instead of coords

--- cython_solver.pyx
+++ cython_solver.pyx
@@ -5,7 +5,7 @@ from libcpp.vector cimport vector
 from libcpp.utility cimport pair
 
 ctypedef pair[int, int] Cell
-ctypedef pair[vector[Cell], int] LittleKillerConstraint
+ctypedef pair[vector[int], int] LittleKillerConstraint
 
 
 cdef vector[int] ODD
@@ -21,26 +21,30 @@ for idx in range(2, 10, 2):
 @cython.boundscheck(False)
 @cython.nonecheck(False)
 cdef class Sudoku:
-    cdef vector[vector[vector[int]]] pos
-    cdef vector[vector[int]] fixed
+    cdef vector[vector[int]] pos
+    cdef vector[int] fixed
     cdef int rec_cnt
     cdef vector[LittleKillerConstraint] killer_contraints
 
     def __init__(self, odd_even_description, little_killer_constraints):
-        self.pos = [[(EVEN, ODD)[int(c)] for c in row] for row in odd_even_description.split()]
-        self.fixed = [[0] * 9 for _ in range(9)]
+        self.pos = [(EVEN, ODD)[int(c)] for c in odd_even_description if c in "01"]
+        self.fixed = [0] * 81
         self.rec_cnt = 0
 
         for start, goal in little_killer_constraints:
             self.add_little_killer_constraint(start, goal)
 
     def add_little_killer_constraint(self, Cell start, int goal):
-        cells = self.little_killer(start)
+        cells = self.get_diag(start)
         self.killer_contraints.push_back(LittleKillerConstraint(cells, goal))
+
+    @staticmethod
+    cdef inline int cell_idx(int row_idx, int col_idx):
+        return row_idx * 9 + col_idx
 
     cdef bint check_constraints(self):
         possible: bint = True
-        cdef vector[Cell] cells
+        cdef vector[int] cells
         cdef int goal
         cdef LittleKillerConstraint contraint
         for contraint in self.killer_contraints:
@@ -50,7 +54,7 @@ cdef class Sudoku:
 
         cdef int row_idx, col_idx, block_idx
         for row_idx in range(9):
-            if not self.check_region(self.fixed[row_idx]):
+            if not self.check_region(self.get_row(row_idx)):
                 return False
         for col_idx in range(9):
             if not self.check_region(self.get_col(col_idx)):
@@ -61,11 +65,18 @@ cdef class Sudoku:
 
         return possible
 
+    cdef vector[int] get_row(self, int row_idx):
+        cdef vector[int] digits
+        digits.reserve(9)
+        for col_idx in range(9):
+            digits.push_back(self.fixed[Sudoku.cell_idx(row_idx, col_idx)])
+        return digits
+
     cdef vector[int] get_col(self, int col_idx):
         cdef vector[int] digits
         digits.reserve(9)
         for row_idx in range(9):
-            digits.push_back(self.fixed[row_idx][col_idx])
+            digits.push_back(self.fixed[Sudoku.cell_idx(row_idx, col_idx)])
         return digits
 
     cdef vector[int] get_block(self, int block_idx):
@@ -74,7 +85,7 @@ cdef class Sudoku:
         digits.reserve(9)
         for row in range(3):
             for col in range(3):
-                digits.push_back(self.fixed[3*row_idx + row][3 * col_idx + col])
+                digits.push_back(self.fixed[Sudoku.cell_idx(3*row_idx + row, 3 * col_idx + col)])
         return digits
 
     cdef bint check_region(self, digits: vector[cython.int]):
@@ -92,12 +103,12 @@ cdef class Sudoku:
             m = max(m, c)
         return m <= 1
 
-    cdef bint check_little_killer(self, vector[Cell] cells, int goal):
+    cdef bint check_little_killer(self, vector[int] cells, int goal):
         cdef vector[int] digits
         digits.reserve(cells.size())
-        cdef Cell xy
-        for xy in cells:
-            digits.push_back(self.fixed[xy.first-1][xy.second-1])
+        cdef int cell_idx
+        for cell_idx in cells:
+            digits.push_back(self.fixed[cell_idx])
 
         cdef int s = 0, i
         for i in digits:
@@ -108,7 +119,7 @@ cdef class Sudoku:
             return True
         return False
 
-    cdef vector[Cell] little_killer(self, Cell start):
+    def get_diag(self, Cell start):
         cdef Cell cur = start, direction
         if start.first == 1:
             direction = Cell(1, -1)
@@ -119,15 +130,15 @@ cdef class Sudoku:
         if start.second == 9:
             direction = Cell(-1, -1)
 
-        cdef vector[Cell] cells
-        while min(cur.first, cur.second) > 0 and max(cur.first, cur.second) < 10:
-            cells.push_back(cur)
+        cells = []
+        while min(cur) > 0 and max(cur) < 10:
+            cells.append(Sudoku.cell_idx(cur.first - 1, cur.second - 1))
             cur = Cell(cur.first + direction.first, cur.second + direction.second)
         return cells
 
     def __repr__(self):
         lines = []
-        for row in self.fixed:
+        for row in chunks(self.fixed, 9):
             line = [''.join('.123456789'[x] for x in chunk)
                     for chunk in chunks(row, 3)]
             lines.append('|'.join(line))
@@ -137,27 +148,27 @@ cdef class Sudoku:
 
     cpdef solve_rec(self, int row = 0, int col = 0):
         self.rec_cnt += 1
-        if row == 0 and col < 4:
+        if row == 0 and col == 3:
             self.show_progress()
         cdef int value
-        for value in self.pos[row][col]:
-            self.fixed[row][col] = value
+        for value in self.pos[Sudoku.cell_idx(row, col)]:
+            self.fixed[Sudoku.cell_idx(row, col)] = value
             if self.check_constraints():
                 if row == col == 8:
                     print(self)
                     continue
                 self.solve_rec(row+(col+1)//9, (col+1)%9)
 
-        self.fixed[row][col] = 0
+        self.fixed[Sudoku.cell_idx(row, col)] = 0
 
     cdef show_progress(self):
         progress = 0.
         full_progress = 1.
         for idx in range(9):
-            digit = self.fixed[0][idx]
+            digit = self.fixed[idx]
             if digit == 0:
                 break
-            max_possibles = len(self.pos[0][idx])
+            max_possibles = len(self.pos[idx])
             full_progress /= max_possibles
             progress += full_progress * ((digit-1) // 2)
 
